dc7c6d7b58e505344b8c626ab79112c268ea1645
michalppp <hm.dt@centrum.cz>
2023-01-09 18:32:40 +0100

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductPaginateComponent } from './product-paginate/product-paginate.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 
 @NgModule({
@@ -22,6 +23,7 @@ import { CartComponent } from './cart/cart.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -31,7 +33,8 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductPaginateComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [
     AppComponent
