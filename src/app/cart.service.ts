b0983219906e0452c2f077f677d56fdea24f838b
michalppp <hm.dt@centrum.cz>
2023-01-09 17:47:55 +0100

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,22 +2,27 @@ import { Injectable } from '@angular/core';
 import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor() { 
+    window.alert('CartService created');
+  }
 
   addToCart(product: Product) {
+    window.alert('Product added');
     this.items.push(product);
   }
 
   getItems() {
+    window.alert('Product returning ' + this.items);
     return this.items;
   }
 
   clearCart() {
+    window.alert('Cart cleared');
     this.items = [];
     return this.items;
   }
