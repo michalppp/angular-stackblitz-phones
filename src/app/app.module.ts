d0f115e3f0b38f5f057ef44bc75d96675db6882a
michalppp <hm.dt@centrum.cz>
2023-01-09 18:03:35 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductPaginateComponent } from './product-paginate/product-paginate.co
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 
+
 @NgModule({
   imports: [
     BrowserModule,
